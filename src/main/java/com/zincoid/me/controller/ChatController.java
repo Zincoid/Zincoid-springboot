d3c8c5c63c5b80bbd686f4d5d5572ac2d1c46e60
Zincoid <2660181154@qq.com>
2026-06-29 21:09:48 +0800

    修正

--- src/main/java/com/zincoid/me/controller/ChatController.java
+++ src/main/java/com/zincoid/me/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.zincoid.me.controller;
 
 import com.zincoid.me.model.ApiResponse;
+import com.zincoid.me.model.enums.Role;
 import com.zincoid.me.model.vo.MessageVO;
 import com.zincoid.me.model.vo.PageVO;
 import com.zincoid.me.service.MessageService;
@@ -19,6 +20,12 @@ public class ChatController {
     public ApiResponse<MessageVO> send(@RequestParam(required = false) String content,
                                         @RequestParam(required = false) String file) {
         return ApiResponse.success(messageService.send(AuthCtx.getUserId(), content, file));
+    }
+
+    @DeleteMapping("/{messageId}")
+    public ApiResponse<Void> delete(@PathVariable Long messageId) {
+        messageService.delete(AuthCtx.getUserId(), messageId, AuthCtx.getRole() == Role.ADMIN);
+        return ApiResponse.success();
     }
 
     @GetMapping("/public")
